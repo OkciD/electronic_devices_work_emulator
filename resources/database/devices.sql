faebc34eaf67b8874240a5ed46bd1498a00a2ed9
OkciD <yury.baykov@yandex.ru>
2017-09-06 12:16:09 +0300

    database edited

--- resources/database/devices.sql
+++ resources/database/devices.sql
@@ -1,4 +1,19 @@
 BEGIN TRANSACTION;
+CREATE TABLE "truth_table" (
+	`device_id`	INTEGER,
+	`socket_id`	INTEGER,
+	`conditions`	TEXT,
+	FOREIGN KEY(`device_id`) REFERENCES device,
+	FOREIGN KEY(`socket_id`) REFERENCES socket
+);
+INSERT INTO `truth_table` (device_id,socket_id,conditions) VALUES (1,1,'0 1'),
+ (1,2,'1 0'),
+ (2,3,'0 0 1 1'),
+ (2,4,'0 1 0 1'),
+ (2,2,'0 0 0 1'),
+ (3,3,'0 0 1 1'),
+ (3,4,'0 1 0 1'),
+ (3,2,'1 1 1 0');
 CREATE TABLE "socket" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT,
@@ -8,32 +23,17 @@ INSERT INTO `socket` (id,name,type) VALUES (1,'X','in'),
  (2,'Y','out'),
  (3,'X1','in'),
  (4,'X2','in');
-CREATE TABLE `device_socket` (
-	`device_id`	INTEGER,
-	`socket_id`	INTEGER,
-	FOREIGN KEY(`device_id`) REFERENCES device,
-	FOREIGN KEY(`socket_id`) REFERENCES socket
-);
-INSERT INTO `device_socket` (device_id,socket_id) VALUES (1,1),
- (1,2),
- (2,3),
- (2,4),
- (2,2),
- (3,3),
- (3,4),
- (3,2);
 CREATE TABLE "device" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`category_id`	INTEGER,
 	`short_name`	TEXT,
 	`full_name`	TEXT,
-	`truth_table`	TEXT,
 	`image_file`	TEXT,
 	FOREIGN KEY(`category_id`) REFERENCES category
 );
-INSERT INTO `device` (id,category_id,short_name,full_name,truth_table,image_file) VALUES (1,1,'NOT','Инвертор',NULL,NULL),
- (2,1,'AND','Элемент 2И',NULL,NULL),
- (3,1,'NAND','Элемент 2И-НЕ',NULL,NULL);
+INSERT INTO `device` (id,category_id,short_name,full_name,image_file) VALUES (1,1,'NOT','Инвертор',NULL),
+ (2,1,'AND','Элемент 2И',NULL),
+ (3,1,'NAND','Элемент 2И-НЕ',NULL);
 CREATE TABLE `category` (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT NOT NULL
